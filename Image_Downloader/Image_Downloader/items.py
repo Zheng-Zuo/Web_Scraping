297df0b400e2b66382785aff11b16a04e23a79e2
Zheng Zuo <52631658+Zheng-Zuo@users.noreply.github.com>
2021-01-27 14:17:44 +0800

    Add files via upload

---
+++ Image_Downloader/Image_Downloader/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+
+class ImageDownloaderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+    movie_names = scrapy.Field(
+        output_process=TakeFirst()
+    )
